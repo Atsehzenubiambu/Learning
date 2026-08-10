99cc01d77fd6c3c747f6ddea5d27ee740997f9d0
Atsehzenubiambu <161712267+Atsehzenubiambu@users.noreply.github.com>
2026-08-10 06:34:04 +0100

    Fix: replace spaces correctly in palindrome check

--- day04/palindrome.py
+++ day04/palindrome.py
@@ -1,6 +1,6 @@
 text = input("Enter a word or a phrase:")
 text = text.lower()
-text = text.replace("","")
+text = text.replace(" ","")
 if text == text[::-1]:
     print("It is a Palindrome.")
 else:
