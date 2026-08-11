1b206088394e1137feac9ec56c378be67cf1112c
Atsehzenubiambu <Atsehzenubiambu>
2026-08-11 18:15:53 +0100

    Grading Script

--- day05/practice.py
+++ day05/practice.py
@@ -1,3 +1,4 @@
+#Task 1: Add Positive Negative and Zero Checker
 number = float(input("Enter a number:"))
 if number > 0:
     print("Positive")
@@ -5,3 +6,16 @@ elif number < 0:
     print("Negative")
 else:
     print("Zero")
+
+#Task 2: Grading Script
+score = float(input("Enter you score:"))
+if score >= 80:
+    print("A")
+elif score >= 70:
+    print("B")
+elif score >= 60:
+    print("C")
+elif score >= 50:
+    print("D")
+else:
+    print("F")
