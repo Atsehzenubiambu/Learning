ef926dceb2d01c33ed309e6732c29b957e0bea2c
Atsehzenubiambu <Atsehzenubiambu>
2026-08-16 19:00:42 +0100

    Multiplication Table

--- day06/practice.py
+++ day06/practice.py
@@ -17,3 +17,8 @@ for number in range (1, 101):
         continue
     print(number)
 
+# Task 03: Multiplication Table
+for i in range(1,11):
+    for j in range(1,11):
+        print(i * j, end="")
+        print()
