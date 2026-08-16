e7f16a9d55861d25ac16fb9d9422b32df7983227
Atsehzenubiambu <Atsehzenubiambu>
2026-08-16 18:40:40 +0100

    Practice wile loop

--- day06/practice.py
+++ day06/practice.py
@@ -1,5 +1,12 @@
 # Task 1: Print 1-20 using for AND while
-# Task 1 a: Using for
+# Task 1 a: Using for loop
+print("Using for loop:")
 for number in range(21):
     print(number)
 
+# Task 1 b: Using while loop
+print("Using while loop")
+number = 1
+while number <= 20:
+    print(number)
+    number+=1
