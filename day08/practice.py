c4215b5973e250f159378c2d5009a331a28d3ee9
Atsehzenubiambu <Atsehzenubiambu>
2026-08-19 06:17:02 +0100

    Practice sorting numbers in ascending and descending order

--- day08/practice.py
+++ day08/practice.py
@@ -5,3 +5,10 @@ movies.remove("Luca")
 movies.reverse()
 
 print("Movies:", movies)
+
+# Task 2: Sort Numbers
+numbers =[1, 5, 3, 8, 4, 2]
+numbers.sort()
+numbers.sort(reverse=True)
+
+print(numbers)
