48c20eae5c2ff9ce0daf924e2fe8bff449bbf9ed
Atsehzenubiambu <Atsehzenubiambu>
2026-08-21 11:12:33 +0100

    Practice set of numbers

--- day09/practice.py
+++ day09/practice.py
@@ -4,3 +4,13 @@ coordinates = (10, 20)
 x, y = coordinates
 print("x:", x)
 print("y:", y)
+
+# Task 2: Set of numbers
+A = {1, 2, 3}
+B = {3, 4, 5}
+# Union
+print(A|B)
+# Intersection
+print(A&B)
+# Difference
+print(A-B)
