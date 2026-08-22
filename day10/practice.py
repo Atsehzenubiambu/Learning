f43f522a874e5761c51e1fb3ddc4e2a24d784526
Atsehzenubiambu <Atsehzenubiambu>
2026-08-22 20:25:44 +0100

    Practice students names and grades

---
+++ day10/practice.py
@@ -0,0 +1,11 @@
+# Task 1: Students Names and Grades
+students = {
+    "Rahim": "A",
+    "Favour": "B",
+    "Zennie": "A",
+    "Alice": "C"
+}
+
+for name, grade in students.items():
+    print(name, grade)
+    
